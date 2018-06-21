e00d93de6245332ee1761ee74397936b9dab48ee
robert-giaquinto <giaquinto.ra@gmail.com>
2018-06-20 22:07:54 -0700

    remove old files

--- scripts/run_authors_dev_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-data_dir="/Users/robert/Local/apt/data/"
-out_dir="/Users/robert/Local/apt/data/out/authors_dev/"
-python -m src.Apt --input_file ${data_dir}test_apt_authors.dat --vocab_file ${data_dir}vocab.dat --out_dir ${out_dir} \
-       --model_file train_authors_dev5.p --method train --lag 20 \
-       --measurement_noise 0.75 --process_noise 0.1 \
-       --em_convergence 1e-3 --var_convergence 1e-4 --cg_convergence 1e-4 \
-       --em_max_iter 12 --num_topics 15 --num_personas 4 --penalty 0.03 \
-       --num_workers 3;
